c621fa6bdd3ad8dab621351192c08083155cf431
monumentale <monumentaleworks@gmail.com>
2024-06-17 14:06:40 +0100

    add email

--- Server.js
+++ Server.js
@@ -3,8 +3,11 @@ const express = require('express');
 const app = express();
 const admin = require('firebase-admin');
 const cors = require('cors'); 
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
 
 app.use(cors());
+app.use(bodyParser.json());
 var serviceAccount = require("./acesbinarytrades-firebase-adminsdk-w65z3-a6af80ebc3.json");
 let PORT = process.env.PORT || 5000
 
@@ -12,6 +15,60 @@ admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://acesbinarytrades-default-rtdb.firebaseio.com"
 });
+
+
+
+app.post('/send-mail', async (req, res) => {
+  try {
+    console.log(req.body);
+    const { user, pass, htmlToSend, email, subject } = req.body;
+
+    // Validate user and password (replace with your validation logic)
+    // if (!user || !pass || !isValidCredentials(user, pass)) {
+    //   return res.status(401).json({ message: 'Invalid credentials' });
+    // }
+
+    const transporter = nodemailer.createTransport({
+      host: 'mail.privateemail.com',
+      port: 25,
+      secure: false,
+      // port: 465,
+      // secure: true,
+      auth: {
+        user:"support@gravityfinances.com",
+        pass:"gravityfinemail100"
+      },
+      // tls: {
+      //   rejectUnauthorized: false // This can be useful for development, but should be avoided in production
+      // }
+    });
+
+    const mailOptions = {
+      from:`Gravityfinances <support@gravityfinances.com>`,
+      to: email,
+      subject:subject,
+      html: `${htmlToSend}`
+    };
+
+    const info = await transporter.sendMail(mailOptions);
+    console.log('Email sent: ' + info.response);
+    res.json({ message: 'Email sent successfully!' });
+  } catch (error) {
+    console.error('Error occurred:', error.message);
+    console.error('Full error:', error);
+    res.status(500).json({ message: 'Error sending email' });
+  }
+});
+
+// Replace this with your actual user and password validation logic
+function isValidCredentials(user, pass) {
+  // Implement your authentication logic here
+  // This example always returns true for demonstration purposes
+  return true;
+}
+
+
+
 app.delete('/api/deleteUser/:uid', async (req, res) => {
   const uid = req.params.uid;
 
@@ -23,6 +80,8 @@ app.delete('/api/deleteUser/:uid', async (req, res) => {
     res.status(500).send('Error deleting user');
   }
 });
+
+
 
 // Your React app can make a DELETE request to /api/deleteUser/:uid
 // Start the server
